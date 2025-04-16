bed04f5c33fac92302c6bbc409b747bb01171ba6
n7khita-r <nikhitaravi123@gmail.com>
2025-04-16 15:54:41 +0530

    commit

--- backend/routes/analytics.py
+++ backend/routes/analytics.py
@@ -26,7 +26,8 @@ async def get_analytics():
     async for item in items_collection.find():
         items.append(item)
     # damm this is the last lab
-    users = ["A1","B2","C3"]
+	# :((
+    users = []
     async for user in users_collection.find():
         users.append(user)
     
@@ -67,4 +68,5 @@ async def get_analytics():
     
     return JSONResponse({
         "stats": stats
+	"chart": f"data:image/png;base64,{image_base64}" 
     })
